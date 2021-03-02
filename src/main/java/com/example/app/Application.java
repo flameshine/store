3214c8596e49bbebf42aa3b1e67c82d89f9ba6c1
Reichemann <excellenceissoon@gmail.com>
2021-03-02 22:57:26 +0200

    Initial commit.

---
+++ src/main/java/com/example/app/Application.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class that launches the application.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
