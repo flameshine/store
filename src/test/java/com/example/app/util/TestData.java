b6452b36e5346dcadef303bf12500ae91e8cf5c5
Reichemann <excellenceissoon@gmail.com>
2021-03-13 14:14:10 +0200

    Fixed all issues with database + minor fixes.

--- src/test/java/com/example/app/util/TestData.java
+++ src/test/java/com/example/app/util/TestData.java
@@ -1,7 +1,6 @@
 package com.example.app.util;
 
 import java.util.List;
-import java.util.Collections;
 
 import org.testng.annotations.DataProvider;
 
@@ -21,13 +20,13 @@ public final class TestData {
     public static Object[][] users() {
         return new Object[][] {
                 {
-                    List.of(new User(1L, "name1", "email1@test", "password1", null, "firstname1", "lastname1", true, new Role(1L, "name1", Collections.emptySet())), new User(2L, "name2", "email2@test", "password2", null, "firstname2", "lastname2", true, new Role(1L, "name1", Collections.emptySet())), new User(3L, "name3", "email3@test", "password3", null, "firstname3", "lastname3", true, new Role(1L, "name1", Collections.emptySet())))
+                    List.of(new User("name1", "email1@test", "password1", null, "firstname1", "lastname1", true, new Role("name1")), new User("name2", "email2@test", "password2", null, "firstname2", "lastname2", true, new Role("name1")), new User("name3", "email3@test", "password3", null, "firstname3", "lastname3", true, new Role("name1")))
                 },
                 {
                     List.of(new User(), new User(), new User())
                 },
                 {
-                    List.of(new User(1L, EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null), new User(2L, EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null), new User(3L, EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null))
+                    List.of(new User(EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null), new User(EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null), new User(EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null))
                 },
         };
     }
@@ -36,13 +35,13 @@ public final class TestData {
     public static Object[][] roles() {
         return new Object[][] {
                 {
-                    List.of(new Role(1L, "name1", Collections.emptySet()), new Role(2L, "name2", Collections.emptySet()), new Role(3L, "name3", Collections.emptySet()))
+                    List.of(new Role("name1"), new Role("name2"), new Role("name3"))
                 },
                 {
                     List.of(new Role(), new Role(), new Role())
                 },
                 {
-                    List.of(new Role(1L, EMPTY_STRING, null), new Role(2L, EMPTY_STRING, null), new Role(3L, EMPTY_STRING, null))
+                    List.of(new Role(EMPTY_STRING), new Role(EMPTY_STRING), new Role(EMPTY_STRING))
                 },
         };
     }
@@ -51,13 +50,13 @@ public final class TestData {
     public static Object[][] user() {
         return new Object[][] {
                 {
-                    new User(1L, "name", "email@test", "password", null, "firstname", "lastname", true, new Role(4L, "name4", Collections.emptySet()))
+                    new User("name", "email@test", "password", null, "firstname", "lastname", true, new Role("name"))
                 },
                 {
                     new User()
                 },
                 {
-                    new User(1L, EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null)
+                    new User(EMPTY_STRING, EMPTY_STRING, EMPTY_STRING, null, EMPTY_STRING, EMPTY_STRING, null, null)
                 },
         };
     }
@@ -66,13 +65,13 @@ public final class TestData {
     public static Object[][] role() {
         return new Object[][] {
                 {
-                    new Role(1L, "name", Collections.emptySet())
+                    new Role("name")
                 },
                 {
                     new Role()
                 },
                 {
-                    new Role(1L, EMPTY_STRING, null)
+                    new Role(EMPTY_STRING)
                 },
         };
     }
