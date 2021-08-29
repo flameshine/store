19e3cd91bed478283855e6263b5c4d007b535247
Reichemann <excellenceissoon@gmail.com>
2021-08-29 12:21:34 +0300

    Laid basics of the shopping cart functionality.

---
+++ src/main/java/com/example/app/exception/NotEnoughProductsInStockException.java
@@ -0,0 +1,14 @@
+package com.example.app.exception;
+
+/**
+ * Exception to indicate that products are out of stock.
+ */
+
+public class NotEnoughProductsInStockException extends Exception {
+
+    public NotEnoughProductsInStockException() {}
+
+    public NotEnoughProductsInStockException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/app/service/impl/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/app/service/impl/security/UserDetailsServiceImpl.java
@@ -22,17 +22,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        var optionalUser = userRepository.findByUsername(username);
-
-        if (optionalUser.isEmpty()) {
-            var message = String.format("User with the username %s not found.", username);
-            throw new UsernameNotFoundException(message);
-        }
-
-        var user = optionalUser.get();
+        var user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException(
+                String.format(
+                    "User with username %s not found",
+                    username
+                ))
+            );
 
         var roleName = user.getRole().getName();
-
         var grantedAuthority = new SimpleGrantedAuthority(roleName);
 
         return new User(user.getUsername(), user.getPassword(), Set.of(grantedAuthority));
