d757c4d4cab023d42de3de5a6de3cd36d2147985
Reichemann <excellenceissoon@gmail.com>
2021-11-01 21:44:19 +0200

    Added more styles + minor improvements.

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,7 +1,7 @@
 package com.example.app;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 /**
  * Main class that launches the application.
@@ -10,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    // TODO: finalize front-end part -> add styles
+    // TODO: finalize front-end part -> add basic styles
+    // TODO: implement admin functionality
+    // TODO: implement CSRF protection
+    // TODO: add currency conversion functionality
     // TODO: review database design
     // TODO: cover all the logic with corresponding unit-tests
     // TODO: review, refactor and optimize everything after the main functionality is implemented
