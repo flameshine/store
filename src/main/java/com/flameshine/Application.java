8f6d2bd1138174bd295ef351c75ec5f0fb271038
Reichemann <excellenceissoon@gmail.com>
2021-12-22 08:58:23 +0200

    TODO added.

--- src/main/java/com/flameshine/Application.java
+++ src/main/java/com/flameshine/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 @SpringBootApplication
 public class Application {
 
+    // TODO: untangle lombok annotations mess
     // TODO: extend admin functionality (add an ability to manipulate with product & user data directly from UI)
     // TODO: add logging
     // TODO: add currency conversion functionality
