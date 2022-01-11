ccde22d13e66342f29d6bbfcfd9f2e060b02baaf
Reichemann <excellenceissoon@gmail.com>
2022-01-11 21:19:28 +0200

    Polishing

--- src/main/java/com/flameshine/store/controller/RegistrationController.java
+++ src/main/java/com/flameshine/store/controller/RegistrationController.java
@@ -5,8 +5,10 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.validation.BindingResult;
 
@@ -41,8 +43,6 @@ public class RegistrationController {
     @PostMapping
     public ModelAndView registration(@Valid User user, BindingResult bindingResult) {
 
-        validator.validate(user, bindingResult);
-
         if (bindingResult.hasErrors()) {
             return new ModelAndView(Constants.REGISTRATION_PATH);
         }
@@ -51,5 +51,10 @@ public class RegistrationController {
 
         return new ModelAndView(Constants.LOGIN_PATH)
             .addObject("message", "User has been registered successfully");
+    }
+
+    @InitBinder
+    private void setValidator(WebDataBinder binder) {
+        binder.setValidator(validator);
     }
 }
