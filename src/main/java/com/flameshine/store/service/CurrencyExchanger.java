8a24224e236e883dc9b83a07a4e9d82b3045cc27
Reichemann <excellenceissoon@gmail.com>
2022-01-23 18:15:19 +0200

    Re-worded Javadoc

--- src/main/java/com/flameshine/store/service/CurrencyExchanger.java
+++ src/main/java/com/flameshine/store/service/CurrencyExchanger.java
@@ -3,7 +3,7 @@ package com.flameshine.store.service;
 import java.math.BigDecimal;
 
 /**
- * Class responsible for exchanging product prices from one currency to another.
+ * Class responsible for exchanging a given amount from one currency to another.
  */
 
 public interface CurrencyExchanger {
