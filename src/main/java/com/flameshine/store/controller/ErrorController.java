7f67ce6c66fb9575d763fc1c95c921d74da39a47
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2023-05-01 19:19:54 +0300

    Update ErrorController.java

--- src/main/java/com/flameshine/store/controller/ErrorController.java
+++ src/main/java/com/flameshine/store/controller/ErrorController.java
@@ -2,9 +2,7 @@ package com.flameshine.store.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.http.HttpStatus;
 import lombok.extern.slf4j.Slf4j;
 
 import com.flameshine.store.util.Constants;
@@ -18,7 +16,6 @@ import com.flameshine.store.util.Constants;
 public class ErrorController {
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected ModelAndView handle(Exception e) {
 
         log.error("An unhandled exception has occurred", e);
